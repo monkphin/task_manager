5c694aaf338c3a6b20096be3c3d15c6fa839a1cf
Darren Burrows <darren.burrows@gmail.com>
2024-09-08 21:25:58 +0000

    added task creation functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, options);
+});
