b8cce2d9e765c8f42a201fd8f30e2e30a933b3a9
Darren Burrows <darren.burrows@gmail.com>
2024-09-08 20:02:12 +0000

    added read functionality

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,14 +10,21 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = Category.query.order_by(Category.category_name).all()
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
-        category = Category(category_name=request.form.get("category_name"))
-        db.session.add(category)
-        db.session.commit()
-        return redirect(url_for("categories"))
+        category_name = request.form.get("category_name")
+        print(f"Received category_name: {category_name}")  # Debugging output
+        if category_name:
+            category = Category(category_name=category_name)
+            db.session.add(category)
+            db.session.commit()
+            return redirect(url_for("categories"))
+        else:
+            # Handle the case where category_name is None
+            return redirect(url_for("add_category"))
     return render_template("add_category.html")
