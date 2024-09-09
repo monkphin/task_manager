44d8899f4aa6f8aab9e1e8cbd229235f7e952c58
Darren Burrows <darren.burrows@gmail.com>
2024-09-09 19:47:33 +0000

    Fix Heroku deployment issue

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -14,7 +14,8 @@ else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
+    
 db = SQLAlchemy(app)
 
 from taskmanager import routes # noqa
